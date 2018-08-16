cb4aa385fc34146d6736caad53a7fc87cbd52bd4
tomjordancc <tomjordan505@gmail.com>
2018-08-16 15:57:25 +1000

    added code for password

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,8 @@
 """Thomas Jordan"""
+password_length = 6
+password = input("Please enter a password: ")
+
+while len(password) < password_length:
+    password = input("Please enter a password: ")
+
+print("*"*len(password))
