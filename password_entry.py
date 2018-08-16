ea4b2ce4d91bc2b1ccbe6c4ea77642a9b043199a
tomjordancc <tomjordan505@gmail.com>
2018-08-16 16:28:10 +1000

    convert code into serperate functions

--- password_entry.py
+++ password_entry.py
@@ -1,8 +1,23 @@
 """Thomas Jordan"""
+
 password_length = 6
-password = input("Please enter a password: ")
 
-while len(password) < password_length:
-    password = input("Please enter a password: ")
 
-print("*"*len(password))
+def main():
+    password = get_password(password_length)
+    print_stars(password)
+
+
+def get_password(password_length):
+    password = input("Please enter a {} character password: ".format(password_length))
+
+    while len(password) < password_length:
+        password = input("Please enter a {} character password: ".format(password_length))
+    return password
+
+
+def print_stars(sequence):
+    print("*" * len(sequence))
+
+
+main()
